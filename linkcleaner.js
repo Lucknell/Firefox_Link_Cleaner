1f4ff03f4336a51b7f85ee5cb35b781632a5ecae
Lucknell <Lucknell@email.com>
2020-09-02 08:13:31 +0000

    Initial commit

---
+++ linkcleaner.js
@@ -0,0 +1,163 @@
+console.log("linkcleaner");
+function setCurrentChoice(result) {
+    defaultFilters = result.filters || "Error";
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
+let getting = browser.storage.sync.get("filters");
+getting.then(setCurrentChoice, onError);
+//creates context menu
+browser.contextMenus.create({
+    id: "newTabText",
+    title: "Clean text link in new tab",
+    contexts: ["selection"], //get selected text
+});
+browser.contextMenus.onClicked.addListener(contextMenuNewTab);
+
+browser.contextMenus.create({
+    id: "curTabText",
+    title: "Clean text link in current tab",
+    contexts: ["selection"], //get selected text
+});
+browser.contextMenus.onClicked.addListener(contextMenuCurTab);
+
+browser.contextMenus.create({
+    id: "curTabLink",
+    title: "Clean link in current tab",
+    contexts: ["link"], //get link
+});
+
+browser.contextMenus.onClicked.addListener(contextMenuCurTab);
+
+browser.contextMenus.create({
+    id: "newTabLink",
+    title: "Clean link in new tab",
+    contexts: ["link"], //get link
+});
+
+browser.contextMenus.onClicked.addListener(contextMenuNewTab);
+
+browser.browserAction.onClicked.addListener(actionButtonAction);
+
+function actionButtonAction(tab) {
+    url = tab.url;
+    url = processLink(url);
+    if (url === undefined) return;
+    browser.tabs.update({ url: url });
+}
+
+function contextMenuNewTab(info) {
+    text = info.selectionText;
+    url = info.linkUrl;
+    url = text !== undefined ? text : url;
+    url = processLink(url);
+    if (url === undefined) return;
+    browser.tabs.create({ url: url });
+}
+
+function contextMenuCurTab(info) {
+    text = info.selectionText;
+    url = info.linkUrl;
+    url = text !== undefined ? text : url;
+    url = processLink(url);
+    if (url === undefined) return;
+    browser.tabs.update({ url: url });
+}
+
+function processLink(url) {
+    if (validURL(url)) {
+        url = decodeHTMLSymbols(url);
+        let temp = url;
+        url = cleanLink(url);
+        if (isEqual(temp, url)) return undefined;
+        url = protocolAdd(url) ? url : 'https://' + url;
+        return url;
+    }
+}
+function validURL(str) {
+    var pattern = new RegExp(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/g);
+    return pattern.test(str);
+}
+
+function protocolAdd(url) {
+    let proto = new RegExp("^(http|https)://", "i");
+    return proto.test(url);
+}
+
+function isEqual(str1, str2) {
+    return str1.toUpperCase() === str2.toUpperCase()
+}
+function cleanLink(args) {
+    console.log("link provided :" + args);
+    if (validURL(args)) {
+        url = decodeHTMLSymbols(args);
+        url = filterLink(url);
+        if (!validURL(url)) return undefined;
+        console.log("filtered link is " + url);
+        if (validURL(url)) {
+            if (isEqual(args, url)) {
+                return args;
+            } else {
+                return url;
+            }
+        }
+        return undefined;
+    }
+    return undefined;
+}
+
+function decodeHTMLSymbols(url) {
+    url += "";
+    return url.replace(/%2F/gi, '/')
+        .replace(/%3A/gi, ":").replace(/%3F/gi, "?")
+        .replace(/%3D/gi, "=").replace(/%26/gi, "&")
+        .replace(/%2B/gi, "+").replace(/%23/gi, "#")
+        .replace(/%7C/gi, "|").replace(/%24/gi, "$")
+        .replace(/%27/gi, "'").replace(/%25/gi, "%");
+}
+
+function filterLink(url) {
+    let getting = browser.storage.sync.get("filters");
+    getting.then(setCurrentChoice, onError);
+    filters = defaultFilters.split("\n");
+    url += "";
+    for (i = 0; i < filters.length; i++) {
+        params = filters[i] + "";
+        filter = params.split(",");
+        if (filter[0] === 'Split') {
+            if (url.includes(filter[1])) {
+                index = filter[3];
+                var temp = url.split(filter[2]);
+                if (validURL(temp[index])) {
+                    url = temp[index];
+                }
+            }
+        }
+        if (filter[0] === 'Replace') {
+            if (url.includes(filter[1])) {
+                if (filter.length == 3) {
+                    var repalce = ""
+                } else {
+                    var replace = filter[3]
+                }
+                if (validURL(url.replace(filter[2], replace))) {
+                    url = url.replace(filter[2], replace);
+                }
+            }
+        }
+        if (filter[0] === 'Prepend') {
+            if (url.includes(filter[1])) {
+                url = filter[2] + url;
+            }
+        }
+        if (filter[0] === 'Append') {
+            if (url.includes(filter[1])) {
+                url = +filter[2];
+            }
+        }
+    }
+    return url;
+}
---
+++ options.js
@@ -0,0 +1,84 @@
+let defaultFilters = "Split,PARM1=,PARM1=,1\n" +
+  "Split,murl=,murl=,1\n" + "Split,clicks.slickdeals.net/i.php?u1=http,u2=,1\n" +
+  "Split,?gclid,&url=,0\n" + "Split,link=,link=,1\n" + "Split,u=,u=,1\n" +
+  "Split,h=,h=,0\n" + "Split,utm_,utm_,0\n" + "Split,&nm_,&nm_,0\n" +
+  "Split,ref=,ref=,0\n" + "Split,u1=,u1=,0\n" + "Split,u2=,u2=,1\n" + "Split,mpre=,ref=,0\n" +
+  "Split,&a=,&a=,0\n" + "Split,q=,q=,1\n" + "Split,token=,token=,0\n" +
+  "Split,&sa=D&,&sa=D&,0\n" + "Split,html_redirect,&html_redirect,0\n" +
+  "Split,&v=,&v=,0\n" + "Split,&mpre=,&mpre=,1\n" +
+  "Split,&event=,&event=,0\n" + "Split,&redir_,&redir,0\n" +
+  "Replace,amp/,amp/s/,amp/s/https://\n" + "Split,amp/s,amp/s/,1\n" + "Replace,amp/,amp/,\n" +
+  "Split,bhphotovideo.com,.html,0\n" + "Append,bhphotovideo.com,.html/\n" +
+  "Replace,1225267-11965372?,url=,url=https://staples.com\n" +
+  "Split,dest_url=,dest_url=,1\n" + "Split,url=,url=,1\n" + "Split,ved=,ved=,0\n" + "Split,src=,src=,0\n" + "Split,source=,source=,0\n" +
+  "Split,&red=,&red=,1\n" + "Split,?pf_rd_r=,?pf_rd_r=,0\n" + "Split,d=sec,d=sec,0\n";
+var finalFilter = "";
+function saveOptions(e) {
+  document.getElementById('heading').innerHTML = 'Saving...';
+  validFilters(document.querySelector("#filters").value)
+  browser.storage.sync.set({
+    filters: finalFilter
+  });
+  document.getElementById('heading').innerHTML = 'Saving complete.'
+  e.preventDefault();
+}
+function resetOptions(e) {
+  document.getElementById('heading').innerHTML = "Default Filters applied";
+  document.getElementById('heading2').innerHTML = "";
+  document.getElementById('filters').value = defaultFilters;
+  e.preventDefault();
+}
+
+function restoreOptions() {
+
+  function setCurrentChoice(result) {
+    document.querySelector("#filters").value = result.filters || defaultFilters;
+  }
+
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+
+  let getting = browser.storage.sync.get("filters");
+  getting.then(setCurrentChoice, onError);
+}
+
+function validFilters(str) {
+  console.log(str);
+  lines = str.split(/\r\n|\r|\n/g);
+  console.log(lines);
+  document.getElementById('heading2').innerHTML = "";
+  for (i = 0; i < lines.length; i++) {
+    filter = lines[i].split(/,/g);
+    if (filter.length === 4 && isEqual(filter[0], "split")) {
+      index = parseInt(filter[3]);
+      if (isNaN(index)) {
+        console.log(index);
+        document.getElementById('heading2').innerHTML += 'error on line ' + (parseInt(i) + 1) + "\n" + filter[3] + " is not a nubmer";
+      }
+      saveFilters(lines[i]);
+    } else if ((filter.length === 4 || filter.length === 3) && isEqual(filter[0], "replace")) {
+      saveFilters(lines[i]);
+    } else if (filter.length === 3 && (isEqual(filter[0], "append") || isEqual(filter[0], "prepend"))) {
+      saveFilters(lines[i]);
+    } else if (lines[i] === "") {
+      document.getElementById('heading2').innerHTML += ' Done!';
+    } else {
+      document.getElementById('heading2').innerHTML += ' error on line ' + (parseInt(i) + 1);
+      console.log("what else is there? \n" + lines[i]);
+    }
+
+  }
+}
+
+function saveFilters(str) {
+  finalFilter += str + "\n";
+  console.log(finalFilter);
+}
+
+function isEqual(str1, str2) {
+  return str1.toUpperCase() === str2.toUpperCase()
+}
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
+document.querySelector("form").addEventListener("reset", resetOptions);
